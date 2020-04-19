6c9b2a27b0c356b30e140545fd5f6e47dc10c2b9
Abdullah-mz <60837981+Abdullah-mz@users.noreply.github.com>
2020-04-20 02:11:10 +0300

    Add files via upload

---
+++ frmCheck.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Project_2
+{
+    partial class frmCheck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chkCoffee = new System.Windows.Forms.CheckBox();
+            this.chkDonut = new System.Windows.Forms.CheckBox();
+            this.chkBrownie = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(45, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(485, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(232, 99);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Back";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chkCoffee
+            // 
+            this.chkCoffee.AutoSize = true;
+            this.chkCoffee.Location = new System.Drawing.Point(74, 23);
+            this.chkCoffee.Name = "chkCoffee";
+            this.chkCoffee.Size = new System.Drawing.Size(69, 21);
+            this.chkCoffee.TabIndex = 2;
+            this.chkCoffee.Text = "Coffee";
+            this.chkCoffee.UseVisualStyleBackColor = true;
+            // 
+            // chkDonut
+            // 
+            this.chkDonut.AutoSize = true;
+            this.chkDonut.Location = new System.Drawing.Point(74, 77);
+            this.chkDonut.Name = "chkDonut";
+            this.chkDonut.Size = new System.Drawing.Size(69, 21);
+            this.chkDonut.TabIndex = 3;
+            this.chkDonut.Text = "Donut";
+            this.chkDonut.UseVisualStyleBackColor = true;
+            // 
+            // chkBrownie
+            // 
+            this.chkBrownie.AutoSize = true;
+            this.chkBrownie.Location = new System.Drawing.Point(74, 121);
+            this.chkBrownie.Name = "chkBrownie";
+            this.chkBrownie.Size = new System.Drawing.Size(78, 21);
+            this.chkBrownie.TabIndex = 4;
+            this.chkBrownie.Text = "Brownie";
+            this.chkBrownie.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkCoffee);
+            this.groupBox1.Controls.Add(this.chkBrownie);
+            this.groupBox1.Controls.Add(this.chkDonut);
+            this.groupBox1.Location = new System.Drawing.Point(151, 146);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(280, 160);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // frmCheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "frmCheck";
+            this.Text = "CheckBox Form";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox chkCoffee;
+        private System.Windows.Forms.CheckBox chkDonut;
+        private System.Windows.Forms.CheckBox chkBrownie;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ frmPic01.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Project_2
+{
+    partial class frmPic01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.BtnImageFromRes = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.picTry = new System.Windows.Forms.PictureBox();
+            this.picTry2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picTry)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTry2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLoadImage.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadImage.Location = new System.Drawing.Point(39, 128);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(162, 87);
+            this.btnLoadImage.TabIndex = 0;
+            this.btnLoadImage.Text = "Image from File";
+            this.btnLoadImage.UseVisualStyleBackColor = false;
+            this.btnLoadImage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnImageFromRes
+            // 
+            this.BtnImageFromRes.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnImageFromRes.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnImageFromRes.Location = new System.Drawing.Point(39, 272);
+            this.BtnImageFromRes.Name = "BtnImageFromRes";
+            this.BtnImageFromRes.Size = new System.Drawing.Size(162, 89);
+            this.BtnImageFromRes.TabIndex = 1;
+            this.BtnImageFromRes.Text = "Image from Resource";
+            this.BtnImageFromRes.UseVisualStyleBackColor = false;
+            this.BtnImageFromRes.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(674, 467);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 81);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Back";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // picTry
+            // 
+            this.picTry.Location = new System.Drawing.Point(6, 53);
+            this.picTry.Name = "picTry";
+            this.picTry.Size = new System.Drawing.Size(272, 300);
+            this.picTry.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picTry.TabIndex = 3;
+            this.picTry.TabStop = false;
+            this.picTry.Click += new System.EventHandler(this.picTry_Click);
+            // 
+            // picTry2
+            // 
+            this.picTry2.Location = new System.Drawing.Point(328, 50);
+            this.picTry2.Name = "picTry2";
+            this.picTry2.Size = new System.Drawing.Size(251, 303);
+            this.picTry2.TabIndex = 4;
+            this.picTry2.TabStop = false;
+            this.picTry2.Click += new System.EventHandler(this.picTry2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.picTry);
+            this.groupBox1.Controls.Add(this.picTry2);
+            this.groupBox1.Location = new System.Drawing.Point(254, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(603, 377);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Picture Boxes";
+            // 
+            // frmPic01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 582);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.BtnImageFromRes);
+            this.Controls.Add(this.btnLoadImage);
+            this.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmPic01";
+            this.Text = "frmPic01";
+            this.Load += new System.EventHandler(this.frmPic01_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picTry)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTry2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button BtnImageFromRes;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox picTry;
+        private System.Windows.Forms.PictureBox picTry2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
